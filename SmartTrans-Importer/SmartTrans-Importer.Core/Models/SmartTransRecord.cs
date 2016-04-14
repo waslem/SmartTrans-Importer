b423bdf13d44f77ad0b4b255cf5b629d5acf7def
Jamie van Walsum <jvw@westnet.com.au>
2016-04-14 16:22:56 +0800

    Added ability to deserialize response to list of SmartTransRecord objects

--- SmartTrans-Importer/SmartTrans-Importer.Core/Models/SmartTransRecord.cs
+++ SmartTrans-Importer/SmartTrans-Importer.Core/Models/SmartTransRecord.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,44 +7,88 @@ using System.Threading.Tasks;
 
 namespace SmartTrans_Importer.Core.Models
 {
-    public class SmartTransRun
-    {
-
-        public string Status;
-
-        public string Agent { get; set; }
-        public DateTime RunDate { get; set; }
-        public List<SmartTransRecord> RecordList { get; set; }
-    }
 
     public class SmartTransRecord
     {
-        public string Order { get; set; }
-        public string DeliveryDate { get; set; }
+        [JsonProperty("OrderNumber")]
+        public string OrderNumber { get; set; }
+
+        [JsonProperty("DeliveryDate")]
+        public DateTime DeliveryDate { get; set; }
+
+        [JsonProperty("ETA")]
         public string ETA { get; set; }
-        public double SiteTime { get; set; }
+
+        [JsonProperty("SiteTime")]
+        public string SiteTime { get; set; }
+
+        [JsonProperty("Arrival")]
         public string Arrival { get; set; }
+
+        [JsonProperty("Departure")]
         public string Departure { get; set; }
-        public int? SiteTimeActual { get; set; }
+
+        [JsonProperty("SiteTimeActual")]
+        public string SiteTimeActual { get; set; }
+
+        [JsonProperty("Customer")]
         public string Customer { get; set; }
+
+        [JsonProperty("Address")]
         public string Address { get; set; }
+
+        [JsonProperty("Suburb")]
         public string Suburb { get; set; }
-        public string Postcode { get; set; }
+
+        [JsonProperty("Postcode")]
+        public int Postcode { get; set; }
+
+        [JsonProperty("State")]
         public string State { get; set; }
+
+        [JsonProperty("Status")]
         public string Status { get; set; }
+
+        [JsonProperty("Reasons")]
         public string Reasons { get; set; }
+
+        [JsonProperty("CustomerRef")]
         public string CustomerRef { get; set; }
+
+        [JsonProperty("TW_start")]
         public string TW_start { get; set; }
+
+        [JsonProperty("TW_end")]
         public string TW_end { get; set; }
+
+        [JsonProperty("Description")]
         public string Description { get; set; }
+
+        [JsonProperty("Comments")]
         public string Comments { get; set; }
-        public object DriverNote { get; set; }
+
+        [JsonProperty("DriverNote")]
+        public string DriverNote { get; set; }
+
+        [JsonProperty("DriverComment")]
         public string DriverComment { get; set; }
+
+        [JsonProperty("Vehicle")]
         public string Vehicle { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
+
+        [JsonProperty("X")]
+        public double? X { get; set; }
+
+        [JsonProperty("Y")]
+        public double? Y { get; set; }
+
+        [JsonProperty("GpsX")]
         public double? GpsX { get; set; }
+
+        [JsonProperty("GpsY")]
         public double? GpsY { get; set; }
+
+        [JsonProperty("Driver")]
         public string Driver { get; set; }
 
     }
--- SmartTrans-Importer/SmartTrans-Importer.Tests/APIQueryTests.cs
+++ SmartTrans-Importer/SmartTrans-Importer.Tests/APIQueryTests.cs
@@ -1,24 +1,42 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SmartTrans_Importer.Core;
+using Newtonsoft.Json;
+using SmartTrans_Importer.Core.Models;
+using System.Collections.Generic;
 
 namespace SmartTrans_Importer.Tests
 {
     [TestClass]
     public class APIQueryTests
     {
-        [TestMethod]
-        public void TestGetDataFromStaging()
-        {
+        //[TestMethod]
+        //public void TestGetDataFromStaging()
+        //{
 
+        //    var Date = new DateTime(2016, 04, 06);
+        //    var Agent = "JVW";
+
+        //    string result = DataQuery.GetRunsheet(Date, Agent);
+
+
+        //    Console.WriteLine(result);
+
+        //}
+
+        [TestMethod]
+        public void TestConvertJsonToObject()
+        {
             var Date = new DateTime(2016, 04, 06);
             var Agent = "JVW";
-
+            
             string result = DataQuery.GetRunsheet(Date, Agent);
 
+            var answer = JsonConvert.DeserializeObject<List<SmartTransRecord>>(result);
+            //var dict = JsonConvert.DeserializeObject<List<SmartTransRecord>>(result);
 
-            Console.WriteLine(result);
-
+           // Console.WriteLine("Finished");
+            Console.WriteLine();
         }
     }
 }
